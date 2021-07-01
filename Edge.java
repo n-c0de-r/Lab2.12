d2946b6174c2cdc32ffe0927fc0bee32ef528cc3
N1co c0des <78597469+n-c0de-r@users.noreply.github.com>
2021-07-01 13:49:50 +0200

    Basic setup during lab

---
+++ Edge.java
@@ -0,0 +1,12 @@
+
+public class Edge {
+	public int startVertex;
+	public int endVertex;
+	public int weight;
+	
+	public Edge (int start, int end, int weight) {
+		startVertex = start;
+		endVertex = end;
+		this.weight = weight;
+	}
+}
---
+++ Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WeightedGraph g = new WeightedGraph();
+		g.setupGraph();
+		g.checkMatrix();
+	}
+
+}
