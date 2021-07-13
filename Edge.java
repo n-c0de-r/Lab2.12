e96fdec132698b32352741e95e316c6cd99d2038
N1co c0des <78597469+n-c0de-r@users.noreply.github.com>
2021-07-13 16:31:46 +0200

    Edge not takes vertices & JDoc updates

--- Edge.java
+++ Edge.java
@@ -1,10 +1,25 @@
+/**
+ * Class creating objects of edges.
+ * Containing vertices and weights.
+ * 
+ * @author	n-c0de-r
+ * @author	AlexanderStae
+ * @version 11.07.2021
+ */
 
 public class Edge {
-	public int startVertex;
-	public int endVertex;
+	public Vertex startVertex;
+	public Vertex endVertex;
 	public int weight;
 	
-	public Edge (int start, int end, int weight) {
+	/**
+	 * Constructor of the class.
+	 * 
+	 * @param start		Starting Vertex.
+	 * @param end		Neighboring Vertex.
+	 * @param weight	Weight between two.
+	 */
+	public Edge (Vertex start, Vertex end, int weight) {
 		startVertex = start;
 		endVertex = end;
 		this.weight = weight;
