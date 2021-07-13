19511402a9fbd4fd3c5500b1ce35d905bba0c6a7
N1co c0des <78597469+n-c0de-r@users.noreply.github.com>
2021-07-13 16:33:22 +0200

    Include name generator & JDoc updates

--- Main.java
+++ Main.java
@@ -1,11 +1,27 @@
+import java.util.ArrayList;
 
+/**
+ * Main running class.
+ * 
+ * @author	n-c0de-r
+ * @author	AlexanderStae
+ * @version	11.07.2021
+ */
 public class Main {
 
+	/**
+	 * Main method running the program.
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		NameGenerator n = new NameGenerator();
+		ArrayList<String> names = n.getNames();
+		
 		WeightedGraph g = new WeightedGraph();
 		g.setupGraph();
 		g.checkMatrix();
 	}
+	
 
 }
