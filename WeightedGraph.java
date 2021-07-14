2551c8a73ef7d1390ef907bf74894a5cca984da4
N1co c0des <78597469+n-c0de-r@users.noreply.github.com>
2021-07-14 13:48:08 +0200

    Random generation, some fixes

--- WeightedGraph.java
+++ WeightedGraph.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Main class that creates weighted graphs
@@ -10,10 +11,10 @@ import java.util.ArrayList;
  */
 public class WeightedGraph {
 	
-	int[][] matrix;
+	private int[][] matrix;
 	
-	ArrayList<Edge> edgeList;
-	ArrayList<Vertex> vertices;
+	private ArrayList<Edge> edgeList;
+	private ArrayList<Vertex> vertices;
 	
 	/**
 	 * Constructor to make a new Graph with
@@ -22,8 +23,7 @@ public class WeightedGraph {
 	 * @param e Number of Edges.
 	 */
 	public WeightedGraph(int v, int e) {
-		generateVertices(v);
-		generateEdges(e);
+		generateMatrix(v, e);
 	}
 	
 	/**
@@ -33,39 +33,39 @@ public class WeightedGraph {
 	}
 	
 	/**
-	 * Checks any given matrix via Dijkstra's Algorithm.
+	 * Checks any given matrix via Dijkstra's Algorithm
+	 * finding the cheapest path from start to goal.
 	 * 
 	 * @param start	The vertex to start path from.
 	 * @param goal	The goal vertex to get to.
 	 */
-	public void checkMatrix(int start, int goal) {		
+	public void cheapestPath(int start, int goal) {
 		//Set the distance of the first vertex to zero.
 		//And make it the next to be checked
 		vertices.get(start).setDistance(0);
 		int nextCheck = start;
 		
 		//Check the respective Vertex and it's connections
-		for (int i=nextCheck; i < vertices.size(); i++) {
+		for (int i = nextCheck; i < vertices.size(); i++) {
 			
 			//If it has been visited, check the next in line
 			if (vertices.get(i).isVisited()) {
 				i++; //Skip current vertex
-				
 			} else {
-				//Set the smallest distance to infinity
+				//Set the smallest distance to goal to infinity
 				int smallestDist = Integer.MAX_VALUE;
-				//Set the 
+				//Set the currently checked vertex
 				Vertex thisVertex = vertices.get(i);
 				
 				//Get the distance of the currently checked vertex.
 				int thisDist = thisVertex.getDistance();
 				
-				for (int j=0; j < vertices.size(); j++) {
+				for (int j = 0; j < vertices.size(); j++) {
 					//Same as above for connected vertices
 					Vertex current = vertices.get(j);
 					
 					//If the calculated distance is less than the current, recalculate it
-					if (matrix[i][j] !=0 && ((thisDist + matrix[i][j]) < current.getDistance()) && !thisVertex.isVisited()) {
+					if (matrix[i][j] != 0 && ((thisDist + matrix[i][j]) < current.getDistance()) && !current.isVisited()) {
 						
 						//Set the checked vortex' predecessor to the main one accordingly
 						//And recalculate the distance to sum of this vertex and the edges weight.
@@ -85,32 +85,19 @@ public class WeightedGraph {
 				i = nextCheck-1;
 			}
 		}
-		getPath(vertices.get(goal));
+		getPath("cheap", vertices.get(start), vertices.get(goal));
 	}
 	
 	/**
-	 * Method that returns the resulting path.
-	 * @param end	The goal vertex.
+	 * Checks any given matrix via Dijkstra's Algorithm
+	 * finding the shortest path from start to goal.
+	 * 
+	 * @param start	The vertex to start path from.
+	 * @param goal	The goal vertex to get to.
 	 */
-	private void getPath(Vertex end) {
-		//After all is done, print out all distances. not really needed.
-//		for (Vertex v : vertices) {
-//			System.out.println("Distance to " + v.getName() + " is " + v.getDistance());
-//		}
+	public void shortestPath(int start, int goal) {
 		
-		//When all is finished print the cheapest path to the end
-		System.out.println("Cheapest path to " + end.getName() + " is " + end.getDistance());
-		String endName = end.getName();
-		String path = endName;
-		String lastName = "";
-		Vertex checking = end;
-		while (checking.getPrevious() != null) {
-			checking = checking.getPrevious();
-			lastName = checking.getName();
-			path = path + " <-- " + lastName;
-			
-		}
-		System.out.println("Path to " + endName + " is " + path);
+		getPath("short", vertices.get(start), vertices.get(goal));
 	}
 	
 	/**
@@ -119,8 +106,43 @@ public class WeightedGraph {
 	 */
 	private void generateEdges(int num) {
 		edgeList = new ArrayList<Edge>();
-		for (int i=0; i < num; i++) {
+		
+		for (int i = 0; i < num; i++) {
+			Random rng = new Random();
+			int v1 = rng.nextInt(vertices.size());
+			int v2 = rng.nextInt(vertices.size());
+			int w = rng.nextInt(42)+1;
 			
+			//If the edge already exists, regenerate numbers
+			while (matrix[v1][v2] != 0 || matrix[v2][v1] != 0) {
+				v1 = rng.nextInt(vertices.size());
+				v2 = rng.nextInt(vertices.size());
+			}
+			edgeList.add(new Edge(vertices.get(v1),vertices.get(v2),w));
+		}
+	}
+	
+	/**
+	 * Generates a new matrix of edges and vertices
+	 * @param v	Number of vertices to create.
+	 * @param e	Number of edges to create.
+	 */
+	private void generateMatrix(int v, int e) {
+		//Create a new quadratic matrix of size of all vertices
+		matrix = new int[v][v];
+		
+		generateVertices(v);
+		generateEdges(e);
+				
+		//Fill the matrix with weights of the edges
+		for (int i = 0; i < e; i++) {
+			Edge currentEdge = edgeList.get(i);
+			Vertex startVertex = currentEdge.startVertex;
+			Vertex endVertex = currentEdge.endVertex;
+			int weight = currentEdge.weight;
+			
+			matrix[vertices.indexOf(startVertex)][vertices.indexOf(endVertex)] = weight;
+			matrix[vertices.indexOf(endVertex)][vertices.indexOf(startVertex)] = weight;
 		}
 	}
 	
@@ -133,7 +155,41 @@ public class WeightedGraph {
 		ArrayList<String> names = n.getNames();
 		
 		vertices = new ArrayList<>();
+		for (int i = 0; i < num; i++) {
+			Random rng = new Random();
+			int r = rng.nextInt(names.size());
+			vertices.add(new Vertex(names.get(r)));
+			names.remove(r);
+		}
 	}
+	
+	/**
+	 * Method that returns the resulting path.
+	 * @param end	The goal vertex.
+	 */
+	private void getPath(String type, Vertex start, Vertex end) {
+		
+		//After all is done, print out all distances. not really needed.
+		for (Vertex v : vertices) {
+			System.out.println("Distance to " + v.getName() + " is: " + v.getDistance());
+		}
+		
+		//When all is finished print the cheapest or shortest path to the end
+		System.out.println("The " + type + "est path from " + start.getName() + " to " + end.getName() + " is: " + end.getDistance());
+		
+		String endName = end.getName();
+		String path = endName;
+		String lastName = "";
+		Vertex checking = end;
+		while (checking.getPrevious() != null) {
+			checking = checking.getPrevious();
+			lastName = checking.getName();
+			path = path + " <-- " + lastName;
+			
+		}
+		System.out.println("The full reverse path is: " + path);
+	}
+
 	
 	/**
 	 * Creates a basic testing graph.
@@ -163,13 +219,13 @@ public class WeightedGraph {
 		edgeList.add(new Edge(vertices.get(0),vertices.get(6),1));
 		edgeList.add(new Edge(vertices.get(1),vertices.get(6),1));
 		edgeList.add(new Edge(vertices.get(4),vertices.get(6),1));
-		edgeList.add(new Edge(vertices.get(0),vertices.get(4),27)); //Shortest path!
+//		edgeList.add(new Edge(vertices.get(0),vertices.get(4),27)); //Shortest path!
 		
 		//Create a new quadratic matrix of size of all vertices
 		matrix = new int[vertices.size()][vertices.size()];
 		
 		//Fill the matrix with weights of the edges
-		for (int i=0; i<edgeList.size(); i++) {
+		for (int i = 0; i<edgeList.size(); i++) {
 			Edge currentEdge = edgeList.get(i);
 			Vertex startVertex = currentEdge.startVertex;
 			Vertex endVertex = currentEdge.endVertex;
