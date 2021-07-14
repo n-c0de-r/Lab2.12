6f8690de072374d4c2323b040b722c1cd8d03edc
N1co c0des <78597469+n-c0de-r@users.noreply.github.com>
2021-07-14 18:44:32 +0200

    implement shortest path method

--- WeightedGraph.java
+++ WeightedGraph.java
@@ -86,51 +86,51 @@ public class WeightedGraph {
 	 * @param goal  The goal vertex to get to.
 	 */
 	public void shortestPath(int start, int goal) {
-//		//Set the distance of the first vertex to zero.
-//		//And make it the next to be checked
-//		vertices.get(start).setDistance(0);
-//		int nextCheck = start;
-//		
-//		//Check the respective Vertex and it's connections
-//		for (int i = nextCheck; i < vertices.size(); i++) {
-//			
-//			//If it has been visited, check the next in line
-//			if (vertices.get(i).isVisited()) {
-//				i++; //Skip current vertex
-//			} else {
-//				//Set the smallest distance to goal to infinity
-//				int smallestDist = Integer.MAX_VALUE;
-//				//Set the currently checked vertex
-//				Vertex thisVertex = vertices.get(i);
-//				
-//				//Get the distance of the currently checked vertex.
-//				int thisDist = thisVertex.getDistance();
-//				
-//				for (int j = 0; j < vertices.size(); j++) {
-//					//Same as above for connected vertices
-//					Vertex current = vertices.get(j);
-//					
-//					//If the calculated distance is less than the current, recalculate it
-//					if (matrix[i][j] != 0 && ((thisDist + matrix[i][j]) < current.getDistance()) && !current.isVisited()) {
-//						
-//						//Set the checked vortex' predecessor to the main one accordingly
-//						//And recalculate the distance to sum of this vertex and the edges weight.
-//						current.setPrevious(thisVertex);
-//						current.setDistance(thisDist + matrix[i][j]);
-//
-//						//re-calibrate the current rows smallest distance, to find the next vertex to check
-//						if (thisDist + matrix[i][j] < smallestDist) {
-//							smallestDist = thisDist + matrix[i][j];
-//							nextCheck = j;
-//						}
-//					}
-//				}
-//				//After all connections are checked, this main vertex is done & visited!
-//				thisVertex.setVisited(true);
-//				//Reduce by one, as auto-increment will correct in the next for-run
-//				i = nextCheck-1;
-//			}
-//		}
+		// Initialize the amount of vertices to check
+		int toVisit = vertices.size();
+
+		// Initialize the start vertex
+		Vertex thisVertex = vertices.get(start);
+		thisVertex.setDistance(0);
+		int thisDist = thisVertex.getDistance();
+
+		int nextCheck = start;
+
+		// While not all are checked
+		while (toVisit != 0) {
+			// Get the current vertex
+			thisVertex = vertices.get(nextCheck);
+			thisDist = thisVertex.getDistance();
+			thisVertex.setVisited(true);
+
+			// Check all neighboring vertices
+			for (int i = 0; i < vertices.size(); i++) {
+				Vertex current = vertices.get(i);
+
+				// If the current vertices sum of distances is lower, update the current vertex
+				if (matrix[nextCheck][i] != 0
+					&& (thisDist + 1) < current.getDistance()
+					&& !current.isVisited()) {
+					current.setPrevious(thisVertex);
+					current.setDistance(1);
+				}
+			}
+
+			int smallestDist = Integer.MAX_VALUE;
+
+			// Find the next unvisited vertex with the smallest distance to check next
+			for (int i = 0; i < vertices.size(); i++) {
+				if (vertices.get(i).isVisited()) {
+					continue;
+				} else {
+					if (vertices.get(i).getDistance() < smallestDist) {
+						smallestDist = vertices.get(i).getDistance();
+						nextCheck = i;
+					}
+				}
+			}
+			toVisit--;
+		}
 		getPath("short", vertices.get(start), vertices.get(goal));
 	}
 
@@ -161,8 +161,22 @@ public class WeightedGraph {
 
 		}
 		System.out.println("The full " + type + "est path is: " + path);
+		System.out.println();
+		
+		//Reset visited vertices after printout for next method
+		resetVisits();
 	}
-
+	
+	/**
+	 * Resets all vertices to unvisited.
+	 */
+	private void resetVisits() {
+		for (Vertex v : vertices) {
+			v.setVisited(false);
+			v.setDistance(Integer.MAX_VALUE);
+		}
+	}
+	
 	/**
 	 * Creates a basic testing graph. Getting from A to E.
 	 */
@@ -187,10 +201,10 @@ public class WeightedGraph {
 		edgeList.add(new Edge(vertices.get(2), vertices.get(3), 5));
 		edgeList.add(new Edge(vertices.get(3), vertices.get(5), 1));
 		edgeList.add(new Edge(vertices.get(4), vertices.get(5), 1));
-		edgeList.add(new Edge(vertices.get(0), vertices.get(6), 1));
+		edgeList.add(new Edge(vertices.get(0), vertices.get(6), 1));//Cheapest path
 		edgeList.add(new Edge(vertices.get(1), vertices.get(6), 1));
 		edgeList.add(new Edge(vertices.get(4), vertices.get(6), 1));
-//		edgeList.add(new Edge(vertices.get(0),vertices.get(4),27)); //Shortest path!
+		edgeList.add(new Edge(vertices.get(0),vertices.get(4),27)); //Shortest path!
 
 		// Create a new quadratic matrix of size of all vertices
 		matrix = new int[vertices.size()][vertices.size()];
