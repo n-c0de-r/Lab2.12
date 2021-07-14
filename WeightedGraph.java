653f338dcd9500a33d0cb707c512b18466c4389a
N1co c0des <78597469+n-c0de-r@users.noreply.github.com>
2021-07-14 16:02:28 +0200

    Split graphs according to task 2

--- WeightedGraph.java
+++ WeightedGraph.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Random;
 
 /**
  * Main class that creates weighted graphs and checks cheapest and shortest
@@ -17,19 +16,10 @@ public class WeightedGraph {
 	private ArrayList<Vertex> vertices;
 
 	/**
-	 * Constructor to make a new Graph with given number of vertices and edges.
-	 * 
-	 * @param v Number of vertices.
-	 * @param e Number of Edges.
-	 */
-	public WeightedGraph(int v, int e) {
-		generateMatrix(v, e);
-	}
-
-	/**
 	 * Empty constructor.
 	 */
 	public WeightedGraph() {
+		testGraph();
 	}
 
 	/**
@@ -95,75 +85,53 @@ public class WeightedGraph {
 	 * @param start The vertex to start path from.
 	 * @param goal  The goal vertex to get to.
 	 */
-//	public void shortestPath(int start, int goal) {
+	public void shortestPath(int start, int goal) {
+//		//Set the distance of the first vertex to zero.
+//		//And make it the next to be checked
+//		vertices.get(start).setDistance(0);
+//		int nextCheck = start;
 //		
-//		getPath("short", vertices.get(start), vertices.get(goal));
-//	}
-
-	/**
-	 * Generates edges for the matrix.
-	 * 
-	 * @param num Number of edges to create.
-	 */
-	private void generateEdges(int num) {
-		edgeList = new ArrayList<Edge>();
-
-		for (int i = 0; i < num; i++) {
-			Random rng = new Random();
-			int v1 = rng.nextInt(vertices.size());
-			int v2 = rng.nextInt(vertices.size());
-			int w = rng.nextInt(42) + 1;
-
-			// If the edge already exists, regenerate numbers
-			while (matrix[v1][v2] != 0 || matrix[v2][v1] != 0) {
-				v1 = rng.nextInt(vertices.size());
-				v2 = rng.nextInt(vertices.size());
-			}
-			edgeList.add(new Edge(vertices.get(v1), vertices.get(v2), w));
-		}
-	}
-
-	/**
-	 * Generates a new matrix of edges and vertices
-	 * 
-	 * @param v Number of vertices to create.
-	 * @param e Number of edges to create.
-	 */
-	private void generateMatrix(int v, int e) {
-		// Create a new quadratic matrix of size of all vertices
-		matrix = new int[v][v];
-
-		generateVertices(v);
-		generateEdges(e);
-
-		// Fill the matrix with weights of the edges
-		for (int i = 0; i < e; i++) {
-			Edge currentEdge = edgeList.get(i);
-			Vertex startVertex = currentEdge.startVertex;
-			Vertex endVertex = currentEdge.endVertex;
-			int weight = currentEdge.weight;
-
-			matrix[vertices.indexOf(startVertex)][vertices.indexOf(endVertex)] = weight;
-			matrix[vertices.indexOf(endVertex)][vertices.indexOf(startVertex)] = weight;
-		}
-	}
-
-	/**
-	 * Generates vertices for the matrix.
-	 * 
-	 * @param num Number of vertices to create.
-	 */
-	private void generateVertices(int num) {
-		NameGenerator n = new NameGenerator();
-		ArrayList<String> names = n.getNames();
-
-		vertices = new ArrayList<>();
-		for (int i = 0; i < num; i++) {
-			Random rng = new Random();
-			int r = rng.nextInt(names.size());
-			vertices.add(new Vertex(names.get(r)));
-			names.remove(r);
-		}
+//		//Check the respective Vertex and it's connections
+//		for (int i = nextCheck; i < vertices.size(); i++) {
+//			
+//			//If it has been visited, check the next in line
+//			if (vertices.get(i).isVisited()) {
+//				i++; //Skip current vertex
+//			} else {
+//				//Set the smallest distance to goal to infinity
+//				int smallestDist = Integer.MAX_VALUE;
+//				//Set the currently checked vertex
+//				Vertex thisVertex = vertices.get(i);
+//				
+//				//Get the distance of the currently checked vertex.
+//				int thisDist = thisVertex.getDistance();
+//				
+//				for (int j = 0; j < vertices.size(); j++) {
+//					//Same as above for connected vertices
+//					Vertex current = vertices.get(j);
+//					
+//					//If the calculated distance is less than the current, recalculate it
+//					if (matrix[i][j] != 0 && ((thisDist + matrix[i][j]) < current.getDistance()) && !current.isVisited()) {
+//						
+//						//Set the checked vortex' predecessor to the main one accordingly
+//						//And recalculate the distance to sum of this vertex and the edges weight.
+//						current.setPrevious(thisVertex);
+//						current.setDistance(thisDist + matrix[i][j]);
+//
+//						//re-calibrate the current rows smallest distance, to find the next vertex to check
+//						if (thisDist + matrix[i][j] < smallestDist) {
+//							smallestDist = thisDist + matrix[i][j];
+//							nextCheck = j;
+//						}
+//					}
+//				}
+//				//After all connections are checked, this main vertex is done & visited!
+//				thisVertex.setVisited(true);
+//				//Reduce by one, as auto-increment will correct in the next for-run
+//				i = nextCheck-1;
+//			}
+//		}
+		getPath("short", vertices.get(start), vertices.get(goal));
 	}
 
 	/**
@@ -198,7 +166,7 @@ public class WeightedGraph {
 	/**
 	 * Creates a basic testing graph. Getting from A to E.
 	 */
-	public void testGraph() {
+	private void testGraph() {
 		// Create an ArrayList of vertices
 		vertices = new ArrayList<>();
 		vertices.add(new Vertex("A"));
